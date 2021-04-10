2660c725779da8f98dc7174acff64d69c2b99556
marcode24 <marco24cruz08@gmail.com>
2021-04-10 16:58:35 -0500

    techniques pages done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-const d = document;
-const header = d.getElementById('header').classList;
+const header = document.getElementById('header').classList;
 window.addEventListener('scroll', function() {
     (window.scrollY > 200) ? header.add('header-scrolled') : header.remove('header-scrolled');
 });
-
 (window.scrollY > 200) ? header.add('header-scrolled') : header.remove('header-scrolled');
