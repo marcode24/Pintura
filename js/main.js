d12c2a18fe16d51e36e315271293a8e9706d7bf4
marcode24 <marco24cruz08@gmail.com>
2021-04-27 03:11:27 -0500

    footer section added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const header = document.getElementById('header').classList;
 window.addEventListener('scroll', function() {
-    (window.scrollY > 200) ? header.add('header-scrolled') : header.remove('header-scrolled');
+    (window.scrollY > 150) ? header.add('header-scrolled') : header.remove('header-scrolled');
 });
-(window.scrollY > 200) ? header.add('header-scrolled') : header.remove('header-scrolled');
+(window.scrollY > 150) ? header.add('header-scrolled') : header.remove('header-scrolled');
